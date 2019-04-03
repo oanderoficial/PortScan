535743295e2e789ae60cfe901cbc04dc7aec88ae
Anderson B Silva/ <32654298+oandersonbsilva@users.noreply.github.com>
2019-04-03 18:21:18 -0300

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -40,7 +40,7 @@ case $menu2 in
   3) Sniffar;;
   4) sites;;
   5) sair;;
-  *) echo -e $red'opicao invalida!'&&sleep 2&&menu;;
+  *) echo -e $red'opicão invalida!'&&sleep 2&&menu;;
 esac
 }
 echo -e "$white"
